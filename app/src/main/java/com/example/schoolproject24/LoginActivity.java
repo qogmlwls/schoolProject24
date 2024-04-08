2e1cba7b14c93d62f9c9ca43b0c9a1ac4258c55a
heejin <qogmlwls7@naver.com>
2024-04-09 01:17:01 +0900

    test

--- app/src/main/java/com/example/schoolproject24/LoginActivity.java
+++ app/src/main/java/com/example/schoolproject24/LoginActivity.java
@@ -1,6 +1,9 @@
 package com.example.schoolproject24;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 import androidx.appcompat.app.AppCompatActivity;
 
@@ -10,5 +13,17 @@ public class LoginActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        Button button = findViewById(R.id.button2);
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(LoginActivity.this,SignUpActivity.class);
+                startActivity(intent);
+            }
+        });
+
+
+
     }
 }
